35fdb7fae6400b1dff34cffa5caf39a57cf039c2
anthousecode <dendomenko@gmail.com>
2018-09-02 16:40:16 +0300

    Add seeds for genres

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,35 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+%w[
+  Action
+  Adventure
+  Animation
+  Biography
+  Comedy
+  Crime
+  Documentary
+  Drama
+  Family
+  Fantasy
+  Film
+  Noir
+  History
+  Horror
+  Music
+  Musical
+  Mystery
+  Romance
+  Sci-Fi
+  Short
+  Sport
+  Superhero
+  Thriller
+  War
+  Western
+].each do |title|
+  Genre.create(title: title)
+end
+
+
 Movie.create([{ title: 'Star Wars', description: 'Lorem ipsum', year: Date.new(2003) }, { title: 'Lord of the Rings', description: 'Lorem ipsum', year: Date.new(2003) }, { title: 'Psycho', description: 'Lorem ipsum', year: Date.new(2003) }, { title: 'It', description: 'Lorem ipsum', year: Date.new(2003) }])
